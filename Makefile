8d51c291ba647a77bd9892e275b291020af7acd0
0xpeIpeI <hokumaru12@gmail.com>
2023-10-06 10:24:47 +0900

    first commit

---
+++ Makefile
@@ -0,0 +1,3 @@
+# webassemblyにコンパイル
+compile-webass:
+	cargo build --target=wasm32-unknown-unknown --release
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,16 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+#[no_mangle]
+pub fn add(a:i32, b:i32) -> i32 {
+    return a + b
+}
+
+#[no_mangle]
+pub fn get_timestamp() -> f64 {
+    return unsafe {
+        date_now()
+    }
+}
+extern  {
+    fn date_now() ->f64;
 }
 
 #[cfg(test)]
@@ -8,7 +19,7 @@ mod tests {
 
     #[test]
     fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+        let result = add(1, 2);
+        assert_eq!(result, 3);
     }
 }
